a707afa8d3b6913ce187f3a2f0c95a98a77da6bd
persio de paula godoy <persiopg@gmail.com>
2022-01-15 19:27:02 -0300

    a

--- js/index.js
+++ js/index.js
@@ -8,10 +8,22 @@ function alertbox(){
     console.log(check.checked);
     if(validacao(nome,tele,email,check.checked)){
         alert("dados enviados");
-    }else{alert("Por Favor preencha todos os campos");}
-    
-    
+    }else{alert("Por Favor preencha todos os campos");}    
 }
+
+function scrollbanner(){
+  scrollSpy('#navsaiba', {
+    sectionClass: '.scrollspy',
+    menuActiveTarget: 'a',
+    offset: 100,
+    smoothScroll: true,
+    smoothScrollBehavior: function(element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+};
+
+
 //validação campo preenchido
 function validacao(a,b,c,d){
     if(a!=""&& b!=""&& c!=""&& d == true ){
@@ -22,16 +34,19 @@ function validacao(a,b,c,d){
 //evento botao
 document.getElementById("btnEnviar").addEventListener("click",alertbox);
 
+document.getElementById("navsaiba").addEventListener("click", scrollbanner);
+
+
+//config simple scrollspy
 window.onload = function () {
   scrollSpy('#navMenu', {
     sectionClass: '.scrollspy',
     menuActiveTarget: 'li > a',
-    offset: 1000,
-    // smooth scroll
+    offset: 100,
     smoothScroll: true,
     smoothScrollBehavior: function(element) {
-      console.log('run "smoothScrollBehavior"...', element)
-      element.scrollIntoView({ behavior: 'smooth' }) // default behavior
+      element.scrollIntoView({ behavior: 'smooth' });
     }
-  })
-}
+  });
+};
+
